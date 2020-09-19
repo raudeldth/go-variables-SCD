36240dc1860f9cca10803f0ecd80187da605fd23
edgardodth <edgardodth@gmail.com>
2020-09-18 19:44:56 -0500

    declarando variables de tipo string de dos maneras distintas

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s1 string = "Hola"
+	var s2 string
+	fmt.Println(s1)
+	fmt.Println(s2)
+	s2 = "Cadena"
+	s3 := "Adios"
+	fmt.Println(s3)
+	fmt.Println(s2)
+}
